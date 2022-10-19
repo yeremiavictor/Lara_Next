9c31792bbc4f00c2da68556615a8a15cdc3a6c99
yeremiavictor <yeremiavictor@gmail.com>
2022-10-19 14:29:54 +0700

    Menampilkan data dari database

---
+++ laravel9api/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    //
+    /**
+     * index
+     * 
+     * @return void
+     */
+
+     public function index(){
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Postss', $posts);
+     }
+}
