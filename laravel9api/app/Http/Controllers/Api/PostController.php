df20abea4035b6a1b8a37d061fadb43749440513
yeremiavictor <yeremiavictor@gmail.com>
2022-10-20 08:09:44 +0700

    Update Data dalam databaase

--- laravel9api/app/Http/Controllers/Api/PostController.php
+++ laravel9api/app/Http/Controllers/Api/PostController.php
@@ -70,4 +70,53 @@ class PostController extends Controller
          //return single post as a resource
          return new PostResource(true, 'Data Post Ditemukan', $post);
       }
+
+      /**
+       * update
+       * 
+       * @param mixed $request
+       * @param mixed $post
+       * @return void
+       */
+
+       public function update(Request $request, Post $post)
+       {
+         //define validation rules
+         $validator = Validator::make($request->all(),[
+            'title'     => 'required',
+            'content'   => 'required',
+         ]);
+
+         //check if validation fails
+         if($validator->fails()){
+            return response()->json($validator->errors(), 442);
+         }
+
+         //check if image not empty
+         if ($request->hasFile('image')){
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/'.$post->image);
+
+            //update post with new image
+            $post->update([
+               'image'     => $image->hashName(),
+               'title'     => $request->title,
+               'content'   => $request->content,
+            ]);
+         } else{
+            //update post without image
+            $post->update([
+               'title'   => $request->title,
+               'content' => $request->content,
+            ]);
+         }
+
+         //return response
+         return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+       }
 }
